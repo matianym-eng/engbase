50bade15e28b73ae3c3fa28b92eed36b7360ba15
Matias Nyman <matias.nyman@citec.com>
2021-04-21 22:53:40 +0300

    started unit module and configured environment file

---
+++ engbase/units.py
@@ -0,0 +1,17 @@
+"""
+This is a module for physical quantity handling, it is essentially a wrapper for pint with som extra functionality.
+
+"""
+from pint import Quantity, UnitRegistry
+import numpy as np
+
+
+
+class Q(Quantity):
+    
+
+    def __init__(self,val, unit) -> None:
+
+        
+
+        pass
